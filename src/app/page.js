e13f35d6e36588aa96e7cefa3437a44760aba4e7
Mohamed Oucharrou <oucharroumohemed@gmail.com>
2025-09-24 01:34:54 +0100

    Implement internationalization support with next-intl; configure locale handling and add English and French translations

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
+import {useTranslations} from 'next-intl';
 
 
 export default function Home() {
-  
+  const t = useTranslations('HomePage');
   return (
     <div>
-      <h1>Welcome to Artware</h1>
+      <h1>{t('title')}</h1>
       
     </div>
   );
